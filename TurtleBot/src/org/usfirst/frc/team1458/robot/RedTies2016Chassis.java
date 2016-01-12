2a57f5b33fef8c087453de574527dfb6d51a590d
BSweeney <mehnadnerd@gmail.com>
2016-01-11 17:49:47 -0800

    Added second Victor to chassis.

--- TurtleBot/src/org/usfirst/frc/team1458/robot/RedTies2016Chassis.java
+++ TurtleBot/src/org/usfirst/frc/team1458/robot/RedTies2016Chassis.java
@@ -7,10 +7,14 @@ public class RedTies2016Chassis implements TurtleTankChassis {
 private static RedTies2016Chassis instance;
 	private TurtleVictor rDrive1;
 	private TurtleVictor lDrive1;
+	private TurtleVictor rDrive2;
+	private TurtleVictor lDrive2;
 	
 	private RedTies2016Chassis() {
 		rDrive1=new TurtleVictor(Constants.RIGHT1VICTORPORT);
 		lDrive1=new TurtleVictor(Constants.LEFT1VICTORPORT);
+		rDrive2=new TurtleVictor(Constants.RIGHT2VICTORPORT);
+		lDrive2=new TurtleVictor(Constants.LEFT2VICTORPORT);
 	}
 	
 	public static RedTies2016Chassis getInstance() {
@@ -23,11 +27,13 @@ private static RedTies2016Chassis instance;
 	@Override
 	public void rightDrive(double power) {
 		rDrive1.set(power);
+		rDrive2.set(power);
 	}
 
 	@Override
 	public void leftDrive(double power) {
 		lDrive1.set(power);
+		lDrive2.set(power);
 	}
 
 	@Override
