bb1680cd24543cff1aa24fa6be858aa2e8d92bcb
BSweeney <mehnadnerd@gmail.com>
2016-02-11 16:38:00 -0800

    Added in class for math manipulation for vision

---
+++ TurtleBot/src/com/team1458/turtleshell/vision/VisionMaths.java
@@ -0,0 +1,19 @@
+package com.team1458.turtleshell.vision;
+
+public class VisionMaths {
+	/**
+	 * Constant for converting y values to theta
+	 */
+	public final static double yViewConstant = 255.8191;
+	/**
+	 * Constant for converting x values to theta
+	 */
+	public final static double xViewConstant = 479.1800255;
+	
+	public static double yToTheta(double y) {
+		return Math.acos(y/yViewConstant);
+	}
+	public static double xToTheta(double x) {
+		return Math.acos(x/xViewConstant);
+	}
+}
