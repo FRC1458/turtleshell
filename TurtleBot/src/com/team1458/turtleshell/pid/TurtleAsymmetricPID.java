56fb0e2a0fc30d40491df4ef87adcafed7f1bd94
BSweeney <mehnadnerd@gmail.com>
2016-02-17 17:28:36 -0800

    Finished TurtleAsymmetricPID

--- TurtleBot/src/com/team1458/turtleshell/pid/TurtleAsymmetricPID.java
+++ TurtleBot/src/com/team1458/turtleshell/pid/TurtleAsymmetricPID.java
@@ -2,23 +2,37 @@ package com.team1458.turtleshell.pid;
 
 import com.team1458.turtleshell.MotorValue;
 
+/**
+ * Class used for synchronising two PID when should be going at different rates.
+ * Assumes they should PROPORTIONALLY go, at percent
+ * 
+ * @author mehnadnerd
+ *
+ */
 public class TurtleAsymmetricPID implements TurtleDualPID {
 	private final TurtlePID apid;
 	private final TurtlePID bpid;
-	private final double kAB;
+	private final double kABA;
+	private final double kABB;
 
 	private double aDist;
 	private double bDist;
 	private double aRate;
 	private double bRate;
 
+	private double aTarget;
+	private double bTarget;
+
 	private MotorValue aLone, bLone;
 
-	public TurtleAsymmetricPID(TurtlePIDConstants kA, TurtlePIDConstants kB, double aTarget, double bTarget, double kAB,
-			double aTolerance, double bTolerance) {
+	public TurtleAsymmetricPID(TurtlePIDConstants kA, TurtlePIDConstants kB, double aTarget, double bTarget,
+			double kABA, double kABB, double aTolerance, double bTolerance) {
 		apid = new TurtlePDD2(kA, aTarget, aTolerance);
 		bpid = new TurtlePDD2(kB, bTarget, bTolerance);
-		this.kAB = kAB;
+		this.aTarget = aTarget;
+		this.bTarget = bTarget;
+		this.kABA = kABA;
+		this.kABB = kABB;
 	}
 
 	@Override
@@ -42,7 +56,9 @@ public class TurtleAsymmetricPID implements TurtleDualPID {
 		aLone = apid.newValue(new double[] { aDist, aRate });
 		bLone = bpid.newValue(new double[] { bDist, bRate });
 
-		return new MotorValue[] { new MotorValue(aLone.getValue()+kAB*0), new MotorValue(bLone.getValue()-kAB*0) };
+		return new MotorValue[] {
+				new MotorValue(aLone.getValue() + kABA * (aLone.getValue() / aTarget - bLone.getValue() / bTarget)),
+				new MotorValue(bLone.getValue() - kABB * (aLone.getValue() / aTarget - bLone.getValue() / bTarget)) };
 	}
 
 }
