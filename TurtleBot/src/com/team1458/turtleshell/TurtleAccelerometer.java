45e7baa050f85bbf20120eba4f10dd8c07554d9a
BSweeney <mehnadnerd@gmail.com>
2016-02-18 17:06:24 -0800

    Added in accelerometer interface

---
+++ TurtleBot/src/com/team1458/turtleshell/TurtleAccelerometer.java
@@ -0,0 +1,14 @@
+package com.team1458.turtleshell;
+
+/**
+ * Interface for Accelerometers
+ * @author mehnadnerd
+ *
+ */
+public interface TurtleAccelerometer {
+	/**
+	 * Get the acceleration measured by the accelerometer
+	 * @return X, Y, Z. Y and Z not guaranteed to exist
+	 */
+	public double[] getAcceleration();
+}
