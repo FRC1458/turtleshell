bef0ce30c7a00ad7a38d0dae075e4f42cbf94af4
mehnadnerd <mehnadnerd@gmail.com>
2017-01-21 10:23:01 -0800

    SmartMotor Created
    Updated TalonSRXCAN. Breaking change. Allows follower mode for the TalonSRX.

--- TurtwigBot/src/com/team1458/turtleshell2/implementations/movement/TurtleTalonSRXCAN.java
+++ TurtwigBot/src/com/team1458/turtleshell2/implementations/movement/TurtleTalonSRXCAN.java
@@ -1,21 +1,32 @@
 package com.team1458.turtleshell2.implementations.movement;
 
 import com.ctre.CANTalon;
-import com.team1458.turtleshell2.interfaces.movement.TurtleMotor;
+import com.ctre.CANTalon.TalonControlMode;
+import com.team1458.turtleshell2.interfaces.movement.TurtleSmartMotor;
 import com.team1458.turtleshell2.util.TurtleMaths;
 import com.team1458.turtleshell2.util.types.MotorValue;
 
-public class TurtleTalonSRXCAN implements TurtleMotor {
+public class TurtleTalonSRXCAN implements TurtleSmartMotor {
 	private final CANTalon v;
 	private final boolean isReversed;
+	private final boolean brakeMode;
 
-	public TurtleTalonSRXCAN(int id, boolean isReversed) {
+	public TurtleTalonSRXCAN(int id, boolean isReversed, boolean brakeMode) {
 		v = new CANTalon(id);
+		v.enableBrakeMode(brakeMode);
 		this.isReversed = isReversed;
+		this.brakeMode=brakeMode;
 	}
-	
+
 	public TurtleTalonSRXCAN(int id) {
-		this(id, false);
+		this(id, false, true);
+	}
+
+	public TurtleTalonSRXCAN(int id, TurtleTalonSRXCAN master) {
+		this(id,false,true);
+		v.changeControlMode(TalonControlMode.Follower);
+		v.set(master.getID());
+
 	}
 
 	@Override
@@ -26,6 +37,10 @@ public class TurtleTalonSRXCAN implements TurtleMotor {
 	@Override
 	public MotorValue get() {
 		return new MotorValue(TurtleMaths.reverseBool(isReversed) * v.get());
+	}
+
+	public int getID() {
+		return v.getDeviceID();
 	}
 
 }
