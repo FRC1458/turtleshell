a936c783e807b69b16163c15c7bfa4db9c371402
asinghani <asinghani@users.noreply.github.com>
2017-01-27 15:49:19 -0800

    worked on climber

--- TurtwigBot/src/org/usfirst/frc/team1458/robot/components/BlastoiseClimber.java
+++ TurtwigBot/src/org/usfirst/frc/team1458/robot/components/BlastoiseClimber.java
@@ -1,11 +1,9 @@
 package org.usfirst.frc.team1458.robot.components;
 
-import com.sun.tools.internal.xjc.Driver;
 import com.team1458.turtleshell2.implementations.movement.TurtleTalonSRXCAN;
 import com.team1458.turtleshell2.interfaces.TurtleComponent;
 import com.team1458.turtleshell2.interfaces.input.TurtleButtonInput;
 import com.team1458.turtleshell2.interfaces.movement.TurtleSmartMotor;
-import com.team1458.turtleshell2.util.types.MotorValue;
 import edu.wpi.first.wpilibj.DriverStation;
 import org.usfirst.frc.team1458.robot.constants.RobotConstants;
 
@@ -20,27 +18,25 @@ public class BlastoiseClimber implements TurtleComponent {
 	private TurtleSmartMotor motor1 = new TurtleTalonSRXCAN(RobotConstants.Climber.MOTOR_PORT);
 
 	private TurtleButtonInput climbButton;
-	private TurtleButtonInput stopButton;
 
 	private boolean climbing;
+
+	public BlastoiseClimber(TurtleButtonInput climbButton) {
+		this.climbButton = climbButton;
+	}
 
 	@Override
 	public void teleUpdate() {
 		if(climbButton.getDown()) {
-			climbing = true;
-		}
-
-		if(climbButton.getDown()) {
-			climbing = false;
-			motor1.set(MotorValue.zero);
-		}
-
-		if(climbing) {
 			motor1.set(RobotConstants.Climber.SPEED);
-			if(motor1.isStalling()) {
-				motor1.set(RobotConstants.Climber.SPEED_LOWER);
-			}
+			climbing = true;
+		} else {
+			climbing = false;
 		}
+	}
+
+	public boolean isClimbing() {
+		return climbing;
 	}
 
 	public boolean timeToClimb() {
