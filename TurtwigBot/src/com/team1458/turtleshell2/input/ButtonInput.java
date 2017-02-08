b5217581c65a71b1b1edec16b52b2fe56dc5d7fb
mehnadnerd <mehnadnerd@gmail.com>
2017-02-08 09:46:38 -0800

    Added in use in TurtleJoystickAxis for InputFunction

--- TurtwigBot/src/com/team1458/turtleshell2/input/ButtonInput.java
+++ TurtwigBot/src/com/team1458/turtleshell2/input/ButtonInput.java
@@ -1,10 +1,25 @@
 package com.team1458.turtleshell2.input;
 
+/**
+ * Interface for button inputs, that can be pressed or not.
+ * @author mehnadnerd & asinghani
+ *
+ */
 public interface ButtonInput extends DigitalInput {
 	public default boolean getButton() {
 		return (this.get() == 1);
 	}
 
+	/**
+	 * Detect a rising edge (Button being pressed down)
+	 * Will not trigger more than once without being released in between.
+	 * @return
+	 */
 	public boolean getDown();
+	/**
+	 * Detect a falling edge (Button being released).
+	 * Will not trigger more than once without being pressed in between.
+	 * @return
+	 */
 	public boolean getUp();
 }
--- TurtwigBot/src/com/team1458/turtleshell2/input/TurtleJoystickAxis.java
+++ TurtwigBot/src/com/team1458/turtleshell2/input/TurtleJoystickAxis.java
@@ -1,6 +1,7 @@
 package com.team1458.turtleshell2.input;
 
 import com.team1458.turtleshell2.util.TurtleMaths;
+import com.team1458.turtleshell2.util.TurtleMaths.InputFunction;
 
 import edu.wpi.first.wpilibj.Joystick;
 
@@ -9,35 +10,46 @@ public class TurtleJoystickAxis implements AnalogInput {
 	private final int axisNum;
 	private final boolean isReversed;
 	private final boolean positive;
+	private final InputFunction func;
 
 	public TurtleJoystickAxis(Joystick j, Joystick.AxisType axis) {
 		this(j, axis.value, false);
 	}
 
-	public TurtleJoystickAxis(Joystick j, int axisNum, boolean isReversed) {
-		this.masterJoystick = j;
-		this.axisNum = axisNum;
-		this.isReversed = isReversed;
-		positive = false;
+	public TurtleJoystickAxis(Joystick j, Joystick.AxisType axis, boolean isReversed) {
+		this(j, axis.value, isReversed);
 	}
 
-	public TurtleJoystickAxis(Joystick j, int axisNum, boolean isReversed, boolean positive) {
-		this.masterJoystick = j;
-		this.axisNum = axisNum;
-		this.isReversed = isReversed;
-		this.positive = positive;
+	public TurtleJoystickAxis(Joystick j, Joystick.AxisType axis, InputFunction func) {
+		this(j, axis.value, false, false, func);
 	}
 
 	public TurtleJoystickAxis(Joystick j, int axisNum) {
 		this(j, axisNum, false);
 	}
 
+	public TurtleJoystickAxis(Joystick j, int axisNum, boolean isReversed) {
+		this(j, axisNum, isReversed, false);
+	}
+
+	public TurtleJoystickAxis(Joystick j, int axisNum, boolean isReversed, boolean positive) {
+		this(j, axisNum, isReversed, positive, InputFunction.identity);
+	}
+
+	public TurtleJoystickAxis(Joystick j, int axisNum, boolean isReversed, boolean positive, InputFunction func) {
+		this.masterJoystick = j;
+		this.axisNum = axisNum;
+		this.isReversed = isReversed;
+		this.positive = positive;
+		this.func = func;
+	}
+
 	@Override
 	public double get() {
 		if (positive) {
-			return ((TurtleMaths.reverseBool(isReversed) * masterJoystick.getRawAxis(axisNum)) + 1.0) / 2.0;
+			return ((TurtleMaths.reverseBool(isReversed) * func.apply(masterJoystick.getRawAxis(axisNum))) + 1.0) / 2.0;
 		}
-		return TurtleMaths.reverseBool(isReversed) * masterJoystick.getRawAxis(axisNum);
+		return TurtleMaths.reverseBool(isReversed) * func.apply(masterJoystick.getRawAxis(axisNum));
 	}
 
 	public TurtleJoystickAxis positive() {
