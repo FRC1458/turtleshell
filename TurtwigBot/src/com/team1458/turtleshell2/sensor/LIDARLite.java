b68715ffd4e9cd5de339461f6e714d9e019938d4
asinghani <asinghani@users.noreply.github.com>
2017-02-16 21:44:56 -0800

    converted lidar to background task

--- TurtwigBot/src/com/team1458/turtleshell2/sensor/LIDARLite.java
+++ TurtwigBot/src/com/team1458/turtleshell2/sensor/LIDARLite.java
@@ -7,6 +7,8 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import java.util.TimerTask;
+
 /**
  * LIDARLite v3 Sensor over I2C interface.
  * 
@@ -16,29 +18,46 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class LIDARLite implements TurtleDistanceSensor {
 	private final I2C ic;
 
-	public LIDARLite(I2C.Port port) {
+	private Distance distance = Distance.error;
+
+	private static Object lock = new Object();
+
+	public LIDARLite(I2C.Port port, long update) {
 		ic = new I2C(port, 0x62);
 
-		// Configure Sensor. See
-		// http://static.garmin.com/pumac/LIDAR_Lite_v3_Operation_Manual_and_Technical_Specifications.pdf
-		ic.write(0x02, 0x80);
-		ic.write(0x04, 0x08);
-		ic.write(0x1c, 0x00);
+		// Configure Sensor. See http://static.garmin.com/pumac/LIDAR_Lite_v3_Operation_Manual_and_Technical_Specifications.pdf
 
+		ic.write(0x02, 0x80); // Maximum number of acquisitions during measurement
+		ic.write(0x04, 0x08); // Acquisition mode control
+		ic.write(0x1c, 0x00); // Peak detection threshold bypass
+
+		new java.util.Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				synchronized (lock) {
+					distance = measureDistance();
+				}
+			}
+		}, update, update);
 	}
 
-	@Override
-	public Distance getDistance() {
+	/**
+	 * Measures distance and puts into distance variable
+	 */
+	public Distance measureDistance() {
 		byte[] high = new byte[]{0x00};
 		byte[] low = new byte[]{0x00};
 		byte[] status = { Byte.MAX_VALUE }; // All bits are 1
 
-		// This is the command for taking distance measurement with bias correction
+		// This is the command for starting distance measurement with bias correction
 		ic.write(0x00, 0x04);
 
 
 		// TODO make this not fail and get stuck in a loop
-		while ((status[0] & 0b0000_0001) == 0b0000_0001) { // need to check if this is correct for LSB
+		/**
+		 * This block waits until the sensor is ready with new data
+		 */
+		while ((status[0] & 0b0000_0001) == 0b0000_0001) { // TODO need to check if this is correct for LSB
 			ic.read(0x01, 1, status); // check status
 			SmartDashboard.putString("Status", Integer.toBinaryString(status[0]));
 		}
@@ -52,9 +71,17 @@ public class LIDARLite implements TurtleDistanceSensor {
 				System.out.println(value);
 				return Distance.createCentimetres(value);
 			}
-
 		}
+
 		return Distance.error;
+	}
+
+	@Override
+	public Distance getDistance() {
+		synchronized (lock) {
+			distance = measureDistance();
+		}
+		return distance;
 	}
 
 	@Override
@@ -79,7 +106,8 @@ public class LIDARLite implements TurtleDistanceSensor {
 	 */
 	private int doubleByteToInt(byte[] byteArray) {
 		// TODO everyone else has a much simpler way to do this, we are over-complicating this
-		return (int) byteArray[0] > 0 ? (byteArray[0] * 256 + Byte.toUnsignedInt(byteArray[1]))
-				: Byte.toUnsignedInt(byteArray[0]) * 256 + Byte.toUnsignedInt(byteArray[1]) - 65536;
+		return (int) byteArray[0] > 0 ?
+				(byteArray[0] * 256 + Byte.toUnsignedInt(byteArray[1])) :
+				Byte.toUnsignedInt(byteArray[0]) * 256 + Byte.toUnsignedInt(byteArray[1]) - 65536;
 	}
 }
